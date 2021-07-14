72febce5e1160e32774220d6c601d456cac99e97
Thinkgoez <57626814+Thinkgoez@users.noreply.github.com>
2021-07-14 14:24:26 +0300

    remove src

--- src/components/common/SimpleComponents/Image.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components/native'
-import { paddingMixin, sizesMixin} from './StylesMixins/Mixins'
-
-const Image = styled.Image`
-  ${sizesMixin}
-  ${paddingMixin}
-`;
-
-export default Image
