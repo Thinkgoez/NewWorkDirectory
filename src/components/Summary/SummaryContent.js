ba9c306b7e79330049471412b0c330d4a38d7051
Thinkgoez <57626814+Thinkgoez@users.noreply.github.com>
2021-08-06 16:52:14 +0300

    13.1, added barcode info block

--- src/components/Summary/SummaryContent.js
+++ src/components/Summary/SummaryContent.js
@@ -1,20 +1,26 @@
 import React from 'react'
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 
 import { StyledView } from '../common/SimpleComponents'
+import { InfoBlock } from '../common/CombinationComponents';
 import { SummaryTab } from './SummaryTab';
 import { CartonScreen } from './TabScreens/CartonScreen';
 import { ArticleScreen } from './TabScreens/ArticleScreen';
+import { useBarcode } from '../../hooks/barcode'
 
 const Tabs = createMaterialTopTabNavigator()
 
 export const SummaryContent = () => {
-    return(
+    const barcodeError = useBarcode()
+    let blockInfoText = 'Pull the trigger to scan a carton barcode'
+    if (barcodeError) blockInfoText = 'Invalid barcode'
+    return (
         <StyledView backgroundColor='#efeff0' flex={1} borderRadius='5px'>
             <Tabs.Navigator tabBar={SummaryTab}>
                 <Tabs.Screen name='Cartons' component={CartonScreen} />
                 <Tabs.Screen name='Articles' component={ArticleScreen} />
             </Tabs.Navigator>
+            <InfoBlock color={!!barcodeError ? '#fff' : '#003556'} text={blockInfoText} borderColor={!!barcodeError ? '#b51145' : '#003556'} fill={!!barcodeError} />
         </StyledView>
     )
 }
