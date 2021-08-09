53aad0d709d331e0898ae257549d103da6354892
Thinkgoez <57626814+Thinkgoez@users.noreply.github.com>
2021-08-09 16:52:20 +0300

    16.1, added modal page

--- src/components/common/SimpleComponents/Button.js
+++ src/components/common/SimpleComponents/Button.js
@@ -14,6 +14,7 @@ const StyledTouchableOpacity= styled.TouchableOpacity`
     ${(({backgroundColor}) => backgroundColor !== undefined ? `background-color: ${backgroundColor};`: '')}
     ${(({border}) => border !== undefined ? `border: ${border};`: '')}
     ${(({borderRadius}) => borderRadius !== undefined ? `border-radius: ${borderRadius};`: '')}
+    ${(({opacity}) => opacity !== undefined ? `opacity: ${opacity};`: '')}
 `
 
 export default StyledTouchableOpacity
