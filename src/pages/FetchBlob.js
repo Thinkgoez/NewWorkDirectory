f75e077ddf75e46f90e7bc84e92cf84943813286
Thinkgoez <57626814+Thinkgoez@users.noreply.github.com>
2021-08-24 13:43:00 +0300

    20.2, fixed pull request comment

--- src/pages/FetchBlob.js
+++ src/pages/FetchBlob.js
@@ -12,17 +12,9 @@ const FetchBlob = () => {
     useEffect(async () => {
         try {
             const res = await RNFetchBlob.fetch('GET', IMAGE_URL,)
-            try {
-                const base64 = await res.base64()
-                try {
-                    await RNFS.writeFile(FILE_PATH, base64, 'base64')
-                    setImageData(FILE_PATH)
-                } catch (err) {
-                    console.log('writeFile error:', err)
-                }
-            } catch (err) {
-                console.log("base64 error", err)
-            }
+            const base64 = await res.base64()
+            await RNFS.writeFile(FILE_PATH, base64, 'base64')
+            setImageData(FILE_PATH)
         } catch (err) {
             console.error('Fetch error:', err)
         }
@@ -30,7 +22,15 @@ const FetchBlob = () => {
     const extension = (Platform.OS === 'android') ? 'file://' : ''
     return (
         <StyledView>
-            {!!imageData && <StyledText textAlign='center' fontSize='20px' fontWeight='bold' textTransform='uppercase' paddingVertical='8px'>hola mericos</StyledText>}
+            {!!imageData &&
+                <StyledText
+                    textAlign='center'
+                    fontSize='20px'
+                    fontWeight='bold'
+                    textTransform='uppercase'
+                    paddingVertical='8px'
+                >hola mericos</StyledText>
+            }
             {!!imageData && <StyledImage source={{ uri: extension + imageData }} width='100%' height='400px' />}
         </StyledView>
     )
