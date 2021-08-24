ab86abfe45d344ae109fee9c9c1da0dff3b8a235
Thinkgoez <57626814+Thinkgoez@users.noreply.github.com>
2021-08-24 14:38:23 +0300

    after second merging fixes

--- src/components/Signature/Signature.js
+++ src/components/Signature/Signature.js
@@ -6,6 +6,7 @@ export const Signature = () => {
     const signature = useRef()
     const saveSign = () => {
         signature.current.saveImage();
+        resetSign()
     }
 
     const resetSign = () => {
